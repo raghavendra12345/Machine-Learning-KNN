74aec49dca40771d9d14f12590ec4b2c859c50ff
Raghavendra Vernekar <rnsvernekar@gmail.com>
2019-06-22 20:05:44 +0530

    Add files via upload
    Machine Learning using Python Coursera .Thanks to Saeed Aghabozorgi sir for his excellent course .

---
+++ knn_customer.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 16:30:49 2019
+
+@author: raghu
+"""
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+
+customer_data=pd.read_csv("D:\Raghavendra\Machine_Learning_codes\customer.csv")
+
+#print(customer_data.info())
+#first 5 rows
+#print(customer_data.head())
+
+print(customer_data['custcat'].value_counts())
+
+
+customer_data.hist(column='income', bins=10)
+
+#To use scikit-learn library, we have to convert the Pandas data frame to a Numpy array:
+X = customer_data[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+X[0:5]
+
+y = customer_data['custcat'].values
+y[0:5]
+
+#normalizing the data
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X[0:5]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+#import library
+from sklearn.neighbors import KNeighborsClassifier
+
+k = 4
+#Train Model and Predict  
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+yhat = neigh.predict(X_test)
+yhat[0:5]
+
+#checking the accuracy
+from sklearn import metrics
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+Ks = 10
+mean_acc = np.zeros((Ks-1))
+std_acc = np.zeros((Ks-1))
+ConfustionMx = [];
+
+for n in range(1,Ks):
+    
+    #Train Model and Predict  
+    neigh = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat=neigh.predict(X_test)
+    mean_acc[n-1] = metrics.accuracy_score(y_test, yhat)
+
+    
+    std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+plt.plot(range(1,Ks),mean_acc,'g')
+plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Nabors (K)')
+plt.tight_layout()
+plt.show()
+
+print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1) 
